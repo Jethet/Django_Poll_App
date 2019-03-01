5064f0b6fda82aca268e785720f284628205db67
jethet <henriette.hettinga@gmail.com>
2019-03-01 12:29:28 +0100

    Alter view using Django template

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
+from .models import Question
 
 # Create your views here.
+"""
+This is the initial index view:
 def index(request):
     return HttpResponse("Hello world. You are at the polls index.")
+In the tutorial, a new index view is created. This also entails changing
+the code because the PAGE DESIGN IS HARD-CODED IN THE VIEW.
+The new index should display the latest 5 poll questions in the system,
+separated by commas and according to publication date (see code def index).
+The tutorial uses a Django template to SEPARATE THE DESIGN from Python by
+creating a template that the view can use:
+"""
+def index(request):
+    latest_question_list = Question.objects.order_by('- pub_date')[:5]
+    output = ','.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
 
 def detail(request, question_id):
     return HttpResponse("You are looking at question %s." % question_id)
