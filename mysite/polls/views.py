3c95d694c6fe00cbe3d661d1de6ca41d85b5d101
jethet <henriette.hettinga@gmail.com>
2019-03-02 18:03:06 +0100

    Raise 404 with get_object_or_404 code

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -20,11 +20,8 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist.")
-    return HttpResponse("You are looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You are looking at the results of question %x."
