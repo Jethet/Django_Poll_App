dac0d76640593fc7ac9cea1790ea0487e41f93cb
jethet <henriette.hettinga@gmail.com>
2019-03-04 12:37:03 +0100

    Create function to create questions plus new test class

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -6,8 +6,28 @@ import datetime
 
 from django.test import TestCase
 from django.utils import timezone
+from django.urls import reverse
 
 from .models import Question
+
+def create_question(question_text, days):
+    """
+    Create a question with the given 'question_text' and published the given
+    number of 'days' offset to now (negative for questions published in the past,
+    positive for questions that have yet to be published):
+    """
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
+
+class QuestionIndexViewTests(TestCase):
+
+    def test_no_questions(self):
+        # If no question exists, an appropriate message is displayed:
+        response = self.client.get(reverse('polls:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+        
 
 class QuestionModelTest(TestCase):
 
